54ab4fcab3c4fdedf7b36eb2fc14bac32721e43d
Ashley Andrade <Noite.Cetafaju@labfies.org.br>
2026-06-22 19:02:57 -0300

    Criando o array de veiculo e declarando as constantes que nele estarão

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
+//PEGANDO ELEMENTOS DO DOM
 const formDados = document.querySelector('#formulario')
 const divInterface = document.querySelector("#Interface")
 
+//DECLARANDO UM ARRAY 
+const veiculo = []
 
 // CAPTURANDO O SUBMIT 
-
-formDados.addEventListener('submit', (evt)=>{
+formDados.addEventListener('submit', (evt) => {
     evt.preventDefault()
 
     const form_dados = new FormData(formDados)
 
+    const veiculo = {
+        modelo: (form_dados.get('modelo')),
+        marca: (form_dados.get('marca')),
+        placa:(form_dados.get('placa')),
+        ano: (form_dados.get('ano')),
+        valorMercado: (form_dados.get('valorMercado')),
+        tipoCombustivel: (form_dados.get('tipoCombustivel')),
+    }
 
-    const modelo = (form_dados.get('modelo'))
-    const marca = (form_dados.get('marca'))
-    const placa = (form_dados.get('placa'))
-    const ano = (form_dados.get('ano'))
-    const valorMercado = (form_dados.get('valorMercado'))
-    const tipoCombustivel = (form_dados.get('tipoCombustivel'))
 
 })
